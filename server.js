e9815eeef75c711e467be0c9be954ba88715be12
CGiardino <carmine.giardino@gmail.com>
2012-11-15 10:54:15 +0100

    refactoring data

---
+++ server.js
@@ -0,0 +1,28 @@
+var csv = require('ya-csv');
+var fs=require('fs');
+
+function Reader(){
+    this.databaseR = csv.createCsvFileReader('./database/data.csv', {
+        'separator': ',',
+        'quote': '"',
+        'escape': '"',
+        'comment': '#'
+    });
+    this.te="by";
+}
+
+Reader.prototype= {
+    read: function(){
+
+        this.databaseR.addListener('data', function(){
+
+
+        })
+    }
+};
+
+
+
+
+
+module.exports=Reader;
--- test/test.js
+++ test/test.js
@@ -1,9 +1,18 @@
-var assert = require("assert")
-describe('Array', function(){
-    describe('#indexOf()', function(){
-        it('should return -1 when the value is not present', function(){
-            assert.equal(-1, [1,2,3].indexOf(5));
-            assert.equal(-1, [1,2,3].indexOf(0));
-        })
+var assert = require("assert");
+Reader= require("./../server");
+describe('Reader', function(){
+    describe('#read()', function(){
+        it('should return -1 when the file do not exist', function(){
+            var reader= new Reader();
+            var array= new Array(new Array());
+            reader.databaseR.addListener('data', function(data){
+                array=data;
+            });
+            reader.databaseR.addListener('end', function(data){
+                console.log(array);
+            });
+
+
+            })
     })
 })
