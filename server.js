ec2e5959c219761241c6d3f9f5b53e8ed586f08f
CGiardino <carmine.giardino@gmail.com>
2012-11-29 14:01:13 +0100

    Google maps added

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var app= express();
 app.configure(function () {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
+    app.use(express.static(__dirname + '/public'));
 });
 
 app.get('/', function (req, res) {
